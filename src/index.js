78f37610cdab5973ce75b2586e2b3104a197f9d5
danhhuy <huydanh0408@gmail.com>
2023-12-29 09:04:40 +0700

    Template engine (handlebars)

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { engine } = require("express-handlebars");
+const morgan = require("morgan");
+
+const app = express();
+const port = 3000;
+
+app.use(morgan("combined"));
+
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./src/resources/views");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
