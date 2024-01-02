e9aa06d49b74caa5a3d7d8750252d9abf3fe90a9
danhhuy <huydanh0408@gmail.com>
2024-01-02 15:19:51 +0700

    POST method

--- src/index.js
+++ src/index.js
@@ -13,6 +13,14 @@ app.use(morgan("combined"));
 app.use(express.static(path.join(__dirname, "public")));
 // app.use(express.static('src/public'))
 
+// Middleware cho phương thức post
+app.use(express.urlencoded( // express có tích hợp sẵn body-parser đối với version mới
+  {
+    extended: true // sử dụng urlencoded cần có options extended của thư viện urlencoded
+  }
+))
+app.use(express.json())
+
 // Template engine
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
@@ -23,8 +31,21 @@ app.get("/", (req, res) => {
 });
 
 app.get("/search", (req, res) => {
-  // console.log(req.query);
+  // console.log(req.query); // có sẵn middleware của express khu dùng query parameters
   res.render("search");
+});
+
+app.post("/search", (req, res) => {
+  console.log(req.body); 
+  /**
+   *  vì body chưa có middleware như query 
+   *  -> cần lấy ra và sử dụng để có thể định nghĩa req.body
+   *  -> sau khi "use urlencoded" đã có thư viện body-parser -> thì có thể sử dụng req.body
+   *      .body-parser sử dụng "qs" (query string) 
+   *          - qs parse obj sang chuỗi (form data truyền dưới dạng obj -> parse sang chuỗi -> submit)
+   */
+
+  res.send(""); // send ra trang blank
 });
 
 app.get("/news", (req, res) => {
